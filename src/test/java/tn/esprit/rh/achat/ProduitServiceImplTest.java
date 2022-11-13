bce5a78b045e199bf7a57481bdd51250b68792c8
ikbel-fridhi2 <ikbel.fridhi@esprit.tn>
2022-11-13 11:10:23 +0100

    ii

--- src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceImplTest.java
@@ -1,7 +1,10 @@
 package tn.esprit.rh.achat;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import org.mockito.ArgumentMatchers;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,6 +24,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -40,23 +46,74 @@ public class ProduitServiceImplTest {
 
 	@InjectMocks
 	ProduitServiceImpl psm;
+	Produit produit = new Produit ((long)1 ,"mpmacpc","macbook",(float)6.3,new Date());
+
+	List<Produit> listProduits = new ArrayList<Produit>() {
+		{
+			add(new Produit((long)2, "dshfds", "jfstj",(float)6.3, new Date()));
+			add(new Produit((long)3, "jfsd", "Mac",(float)22.1, new Date()));
+		}
+	};
 
 	@Mock
 	ProduitRepository pr;
 
 	@Test
-	public void testRetrieveAllReglements() {
-		l.info("testRetrieveAllReglements()");
-		List<Produit> listReglements = ps.retrieveAllProduits();
-		Assertions.assertEquals(0, listReglements.size());
-		l.info("Out methode testRetrieveAllReglements()");
+	public void testRetrieveAllProduit() {
+		l.info("testRetrieveAllProduits()");
+		List<Produit> listProduits = ps.retrieveAllProduits();
+		Assertions.assertEquals(0, listProduits.size());
+		l.info("Out methode testRetrieveAllProduits()");
+	}
+
+
+
+	@Test
+	public void testAddProduit() {
+		/*l.info("testAddProduit()");
+		Mockito.when(pr.findById(Mockito.anyLong())).thenReturn(Optional.of(produit));
+		Produit produit1 = psm.retrieveProduit(2L);
+		Assertions.assertNotNull(produit1);
+		l.info("Out methode testAddProduit()");
+
+		 */
+
+
+		Mockito.when(pr.save(ArgumentMatchers.any(Produit.class))).thenReturn(produit);
+		Produit savedPR= psm.addProduit(produit);
+
+		assertNotNull(savedPR.getLibelleProduit());
+
+		assertTrue(savedPR.getPrix()>0);
+
+		psm.deleteProduit(savedPR.getIdProduit());
 	}
 
 
 
 
-	
-	/*@Test
+
+	@Test
+	public void testRetrieveProduit() {
+		l.info("testRetrieveProduit()");
+		Mockito.when(pr.findById(Mockito.anyLong())).thenReturn(Optional.of(produit));
+		Produit produit1 = psm.retrieveProduit(2L);
+		Assertions.assertNotNull(produit1);
+		l.info("Out methode testRetrieveProduit()");
+	}
+
+
+	@Test
+	public void testDeleteStock() {
+		Produit p = new Produit ((long)5 ,"lll","lll",(float)6.3,new Date());
+		p.setIdProduit(Long.valueOf(3));
+		psm.addProduit(p);
+		psm.deleteProduit(p.getIdProduit());
+		assertNull(psm.retrieveProduit(p.getIdProduit()));
+	}
+
+	/*
+	@Test
 	public void addProduitTest () 
 	{
 		//Produit p =new Produit ((long)1 ,"ghada","yosra",(float)12.2);
@@ -64,6 +121,7 @@ public class ProduitServiceImplTest {
 		produitRepository.save(p2);
 		
 	}
+
 	
 	@Test 
 	public void retrieveProduitTest()
@@ -106,6 +164,7 @@ public class ProduitServiceImplTest {
 		}
 		
 	}*/
+
 
 	
 	
