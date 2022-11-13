3a66a5e1b9235f45556840bdc459cb4d1f5d7fa7
unknown <cah44416@gmail.com>
2022-11-13 14:30:43 +0100

    1311

--- src/test/java/tn/esprit/rh/achat/test/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/test/StockServiceImplTest.java
@@ -3,6 +3,8 @@ package tn.esprit.rh.achat.test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,54 +21,32 @@ public class StockServiceImplTest {
 
 	@Autowired
 	IStockService stock;
-	@Autowired
-	StockRepository strepo;
-	
-	//@Mock private StockRepository stockrepo;
 	
 	
-	//@Test
-	//public void TestGetAllStock() {
-		//when(stock.retrieveAllStocks());
-		//verify(strepo)findAll();
-	//}
-	
-	
-	
-	@Test
-    void TestAddSecteurActivite() {
-		
-	Stock s = stock.addStock(Stock.builder()
-				
-				.libelleStock("caasd")
-				.qte(546)
-				.qteMin(1)
-				.build());
-	assertNotNull(s);
-	assertNotNull(s.getIdStock());	
-   }
 	
 	
 	
 	
 	@Test
-	void TestretrieveAllStocks(){
+	void testAddStock() {
+		List<Stock> stocks = stock.retrieveAllStocks();
+		int expected=stocks.size();
+		Stock s = new Stock("stock test",10,100);
+		Stock savedStock= stock.addStock(s);
+
+		assertEquals(expected+1, stock.retrieveAllStocks().size());
+		assertNotNull(savedStock.getLibelleStock());
 		
-		List<Stock> l = stock.retrieveAllStocks();
-		for(Stock s : l) {
-			System.out.println("Stock : " + s.getIdStock());
-			System.out.println("Stock : " + s.getLibelleStock());
-			System.out.println("Stock : " + s.getQte());
-			System.out.println("Stock : " + s.getQteMin());
-			System.out.println("**********************************");
-			}
-		//assertEquals(strepo.findAll().size(),l.size());
-		System.out.println("Quantités de Stock : " + l.size());
+
+	}
 	
 	
-		assertThat(l).hasSize(strepo.findAll().size());
-		
 	
+	
+	@Test
+	public void testRetrieveAllStocks() {
+		List<Stock> listStocks = stock.retrieveAllStocks();
+		Assertions.assertEquals(0, listStocks.size());
 	}
 	
 	@Test
@@ -76,20 +56,20 @@ public class StockServiceImplTest {
 		System.out.println("ID = " +exist.getIdStock());
 		assertNotNull(exist);
 }
-	
-	//@Test
-	//void TestdeleteStock(){
-		
-		//stock.deleteStock((long) 2);
-		
-//}
-	
-	
-	
-	
-	
-	
+	@Test
+	void testDeleteStock() {
+		Stock s = new Stock("stock test",30,60);
+		Stock savedStock= stock.addStock(s);
+		stock.deleteStock(savedStock.getIdStock());
+		assertNull(stock.retrieveStock(savedStock.getIdStock()));
+	}
 }
+	
+	
+	
+	
+	
+
 
 	
 	
