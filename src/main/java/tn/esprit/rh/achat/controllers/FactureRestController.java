157ea0aff19ea1fbe36c4b07d0aeaef8b41f41e0
sirine47 <sirine.souayah@esprit.tn>
2022-11-14 14:48:47 +0100

    corrigerbug

--- src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des factures")
 @RequestMapping("/facture")
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class FactureRestController {
 
     @Autowired
